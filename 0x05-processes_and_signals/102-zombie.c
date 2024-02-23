2ffe767d3fdb5d62db55a2a4c168186e01ae4f62
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-23 22:54:28 +0200

    task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * infinite_while - infinty loop
+ * Return: 0
+ * Ashraf Atef
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - craete a zombies process
+ * Return: 0
+ * Ashraf Atef
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		int pid = fork();
+
+		if (pid)
+			printf("Zombie process created, PID: %d\n", pid);
+		else
+			infinite_while();
+	}
+	return (0);
+}
