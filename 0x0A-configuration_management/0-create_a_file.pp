5ed215ce7171a01a9264364cff523a2be62ce313
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-19 22:10:57 +0200

    task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create file in /tmp
 
-file {'/tmp/school':
-    ensure  => file,
-    content => 'I love Puppet',
+file { '/tmp/school' :
+    path    => '/tmp/school',
     mode    => '0744',
-    owner   => www-data,
-    group   => www-data,
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
