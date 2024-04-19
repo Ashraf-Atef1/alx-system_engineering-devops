e9586cdfeb941e647166a9fd3ef423731fbbb658
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-20 01:19:06 +0200

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# Install an especific version of flask (2.1.0)
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/bin', '/usr/bin'],
-  environment => [],
-  require     => Package['python3-pip'],
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
