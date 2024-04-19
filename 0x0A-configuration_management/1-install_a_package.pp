ef1c199819c1577949e9f8b3c82c3ec719aa63e1
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-20 01:13:45 +0200

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Install an especific version of flask (2.1.0)
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/bin', '/usr/bin'],
+  environment => [],
+  require     => Package['python3-pip'],
 }
