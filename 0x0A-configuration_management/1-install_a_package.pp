ece1a0109b033224d53b57dc6772d408afc858f5
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-20 00:57:34 +0200

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,17 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
 package { 'python3-pip':
   ensure => installed,
 }
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/bin', '/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
+
+# Optional: Ensure flask is globally accessible
+file { '/usr/local/bin/flask':
+  ensure  => link,
+  target  => '/usr/local/bin/flask',
+  require => Exec['install_flask'],
 }
