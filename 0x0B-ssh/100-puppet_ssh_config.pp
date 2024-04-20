6d1f2c02b501324fa51174f81681f5461a2b2104
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-20 14:21:26 +0200

    task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,15 @@
 # Changes to config Puppet
 
+include stdlib
+
 file_line { 'refuse_to_authenticate_using_a_password':
-  path => '/etc/ssh/ssh_config',
-  line => '  PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '  PasswordAuthentication no',
 }
 
 file_line { 'IdentityFile':
-  path => '/etc/ssh/ssh_config',
-  line => '  IdentityFile ~/.ssh/school',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '  IdentityFile ~/.ssh/school',
 }
