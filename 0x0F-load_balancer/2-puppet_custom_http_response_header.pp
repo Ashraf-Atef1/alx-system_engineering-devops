699956dee6104f96414bece7058c9950c9ad27c5
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-04-29 23:39:50 +0300

    solve task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,26 @@
 #!/usr/bin/pup
 # Install Nginx web server (w/ Puppet)
 
-package { 'nginx':
-  ensure => installed,
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
 exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
   before      => Exec['restart Nginx'],
 }
+
 exec { 'restart Nginx':
   provider => shell,
   command  => 'sudo service nginx restart',
-}
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
 }
