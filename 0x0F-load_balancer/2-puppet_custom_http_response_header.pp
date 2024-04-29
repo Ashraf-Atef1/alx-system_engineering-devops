30211255830f27583976bde9866674459b7e112b
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-04-30 00:10:16 +0300

    solve task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,10 @@
 #!/usr/bin/pup
 # Install Nginx web server (w/ Puppet)
 
-package { 'nginx':
-  ensure => installed,
-}
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-file_line { 'default':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file_line { 'add-header_X-Served-By':
-  ensure      => 'present',
-  path        => '/etc/nginx/sites-available/default',
-  environment => ["HOST=${hostname}"],
-  after       => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  line        => "add_header X-Served-By ${HOST};",
-}
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
