1bc95e927fc833c3a9581a796a424cb113e41184
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-04-29 23:36:38 +0300

    solve task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -25,7 +25,12 @@ file_line { 'error404':
 exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "26i\	add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
 service { 'nginx':
   ensure  => running,
