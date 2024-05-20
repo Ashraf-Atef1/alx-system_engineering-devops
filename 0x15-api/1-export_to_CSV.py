7d1a2a7c1bf78debc62e216319cef6533f9150fc
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-20 19:11:00 +0300

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that, using a REST API, for a given employee ID,
+returns information about his/her and export data in the CSV format."""
+from json import loads
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    user = loads(user.text)
+    all_tasks = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
+    all_tasks = loads(all_tasks.text)
+    with open(f"{argv[1]}.csv", "w") as f:
+        for task in all_tasks:
+            f.write(f'"{user['id']}","{user['username']}",\
+"{task['completed']}","{task['title']}"\n')
