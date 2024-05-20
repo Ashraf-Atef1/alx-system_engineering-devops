ba2268c542f2232f54f46885939cc93d9389ee7b
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-20 19:28:04 +0300

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-from json import loads
+"""Python script that, using a REST API, for a given employee ID,
+returns information about his/her and export data in the CSV format."""
+import csv
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     user = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
-    user = loads(user.text)
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+        ).json()
     all_tasks = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
-    all_tasks = loads(all_tasks.text)
+        f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+        ).json()
     with open(f"{argv[1]}.csv", "w") as f:
+        csv_file = csv.writer(f, quoting=csv.QUOTE_ALL)
         for task in all_tasks:
-            f.write(f'"{user['id']}","{user['username']}",\
-"{task['completed']}","{task['title']}"\n')
+            csv_file.writerow([user['id'], user['username'],
+                              task['completed'], task['title']])
