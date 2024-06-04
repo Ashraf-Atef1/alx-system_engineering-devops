800c2fc125d3b34bfeb829dc284f5c42ef01ecab
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-06-05 01:20:52 +0300

    task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+module to get the top ten hot posts for a given subreddit
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    function that queries the Reddit API and prints the titles of the first 10
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        after= req.json().get("data").get("after")
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        if after:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    else:
+        return hot_list
