be766840ff7f77a40cd16aaf3df7736ed4c53d81
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-06-05 00:51:53 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,9 @@ def number_of_subscribers(subreddit):
     """
     function that queries the Reddit API and returns the number of subscribers
     """
-    reqest = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                          headers={"User-Agent": "Custom"})
+    reqest = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"})
 
     if reqest.status_code == 200:
         return reqest.json().get("data").get("subscribers")
