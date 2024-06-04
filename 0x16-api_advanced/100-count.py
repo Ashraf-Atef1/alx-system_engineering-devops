df30fadca5d10da65c052a791fa2cb2b0247ecbd
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-06-05 01:53:34 +0300

    task 2

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+module to get the top ten hot posts for a given subreddit
+"""
+import requests
+
+
+def count_words(subreddit, word_list, after=None, counter={}):
+    """
+    function that queries the Reddit API and prints the titles of the first 10
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+    if counter == {}:
+        for word in word_list:
+            counter[word.lower()] = 0
+
+    if req.status_code == 200:
+        after = req.json().get("data").get("after")
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            counter += count_words(title, word_list.split())
+            for word in word_list:
+                counter[word.lower()] += title.lower().split().count(word.lower())
+        if after:
+            return count_words(subreddit, word_list, after, counter)
+        print(counter)
+    else:
+        return None
